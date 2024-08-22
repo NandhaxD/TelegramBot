468a621c6edc068ea3c6c9de4d11a408e963555d
|| 𝗡𝗔𝗡𝗗𝗛𝗔 || - நந்தா <89440790+NandhaxD@users.noreply.github.com>
2024-08-22 15:38:53 +0530

    Update channelforwardContentbot.py

--- example/channelforwardContentbot.py
+++ example/channelforwardContentbot.py
@@ -1,1 +1,35 @@
 
+
+# requirements.txt
+# >>> https://github.com/KurimuzonAkuma/pyrogram/archive/dev.zip 
+# >>> tgcrypto
+
+
+
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+from pyrogram import Client, filters
+
+
+app = Client(
+name="Nandhabot",
+api_id=12345,
+api_hash="api_hash",
+bot_token="7508215693:AAEorpF2EkWbc7BmzX7rIkNh81Yo7HUIq1k"
+)
+
+
+channel_id = -1002168230403
+to_channel_id= -1002201203404
+
+
+@app.on_message(filters.chat(channel_id))
+async def forwarder(bot, message):
+      m = message
+      await m.forward(to_channel_id)
+      
+      
+
+app.run()
