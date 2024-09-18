8c25e84a48badb864e1266c3210a725171993db7
Nandhagopal, K <89440790+NandhaxD@users.noreply.github.com>
2024-09-18 21:03:07 +0530

    Update customFilterEg.py

--- pyrogram/example/customFilterEg.py
+++ pyrogram/example/customFilterEg.py
@@ -1,12 +1,12 @@
 
 from pyrogram import filters, Client
 
-#Let's create a filter that only return True when the user first has '#nandha' hashtag.
+#Let's create a filter that only return True when the user first has tag in their name.
 
 
 hashtag: str = '#nandhabots'
 
-def _hashtag_filter(filter, client, message): return hashtag in message.from_user.first_name
+def _hashtag_filter(filter, client, message) -> bool: return hashtag in message.from_user.first_name
 
 hashtag = filters.create(_hashtag_filter)
 
@@ -16,8 +16,8 @@ hashtag = filters.create(_hashtag_filter)
 
 # Example to making a unique filter
 
-def hashtag(tag: str):
-     def _hashtag_filter(filter, client, message):
+def hashtag(tag: str) -> filters:
+     def _hashtag_filter(filter, client, message) -> bool:
          return tag in message.from_user.first_name
      return filters.create(_hashtag_filter)
 
